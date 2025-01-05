17763f41b79eea0c80b79cecf285560a74b76910
DeekshiN <deekshidt18@gmail.com>
2025-01-06 00:57:39 +0530

    pushing add binary and sqrt x problems

--- easy/add_binary.py
+++ easy/add_binary.py
@@ -21,3 +21,31 @@ a and b consist only of '0' or '1' characters.
 Each string does not contain leading zeros except for the zero itself.
 
 """
+
+
+def add_binary(a, b):
+    a = a[::-1]
+    b = b[::-1]
+
+    result = []
+    carry = 0
+
+    for i in range(max(len(a), len(b))):
+        bit_a = int(a[i]) if i < len(a) else 0
+        bit_b = int(b[i]) if i < len(b) else 0
+
+        total = bit_a + bit_b + carry
+        result.append(str(total % 2))
+        carry = total // 2
+
+    if carry:
+        result.append(str(carry))
+
+    return ''.join(result[::-1])
+
+
+a = "11"
+b = "1"
+
+bin_result = add_binary(a, b)
+print(bin_result)
